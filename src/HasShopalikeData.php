d67551d185ef5c0a601ad65d6c56d4a713c3130f
seszcze <sebastian@ably.pl>
2016-12-23 12:36:42 +0100

    assocaite with shopalike

--- src/HasShopalikeData.php
+++ src/HasShopalikeData.php
@@ -4,6 +4,23 @@ namespace Lari\Shopalike;
 
 trait HasShopalikeData
 {
+    /**
+     * @param int  $category
+     * @param null $name
+     */
+    public function associateWithShopalike(int $category, $name = null)
+    {
+        $name = $name ?? $this->name;
+        
+        return $this->shopalike()->create([
+            'name'        => $name,
+            'category_id' => $category
+        ]);
+    }
+
+    /**
+     * @return mixed
+     */
     public function shopalike()
     {
         return $this->morphOne(ShopalikeItem::class, 'linkable');
